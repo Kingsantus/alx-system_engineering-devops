fdfc4db835732f2f8cbadbbf2b96bac30a9e8fdc
kingsantus <kingsantusasogwa@gmail.com>
2024-01-17 08:09:40 -0800

    correcting PHP

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,28 +1,5 @@
-# 0-strace_is_your_friend.pp
-
-# Define the Apache service
-service { 'apache2':
-  ensure => 'running',
-  enable => true,
-}
-
-# Run strace and identify the issue
-exec { 'strace_apache':
-  command  => 'sudo strace -f -o /tmp/apache_strace.log curl -sI 127.0.0.1',
-  require  => Service['apache2'],
-  creates  => '/tmp/apache_strace.log', # Only run if the log file doesn't exist
-}
-
-# Analyze strace output and fix the identified issue
-exec { 'fix_apache_issue':
+# detecting error
+exec { 'Fix typo in filename':
   command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-  require  => Exec['strace_apache'],
+  provider => shell,
 }
-
-# Ensure Apache service is restarted after the fix
-service { 'apache2':
-  ensure  => 'running',
-  enable  => true,
-  require => Exec['fix_apache_issue'],
-}
-
